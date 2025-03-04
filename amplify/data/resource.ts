ea9bec5dc4661bfa00a0ad705e9aab593b689946
samjules <samjulander@gmail.com>
2025-03-04 13:36:08 -0900

    .

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -2,15 +2,14 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 const schema = a.schema({
   Listing: a.model({
-    id: a.id(), // Add a unique identifier
+    id: a.id(),
     Bed: a.string(),
     Bath: a.string(),
     Title: a.string(),
     Agent: a.string(),
     Price: a.string(),
     CoverPhoto: a.url(),
-  })
-  .authorization(allow => [allow.guest()])
+  }).authorization(allow => [allow.guest()])
 });
 
 export type Schema = ClientSchema<typeof schema>;
