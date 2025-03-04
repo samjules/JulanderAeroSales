7b68ecb3e616fc1ebb414a808b9f9c44da1f38c4
samjules <samjulander@gmail.com>
2025-03-03 20:16:50 -0900

    added new table to database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
+import "@aws-amplify/ui-react/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+   
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+     
+       <body className={inter.className}>{children}</body>
+      
     </html>
   );
 }
